95d73994bf98e847de923f8a614bcec642ebfb6c
André Fernando <andre.engetronica@gmail.com>
2023-10-02 23:04:34 -0300

    Executando a task antes de encher a fila

--- main/main.c
+++ main/main.c
@@ -21,19 +21,19 @@ void comTask(void * params){
     }
 }
 
-//void procTask(void * params){
-//    while(1){
-//        int receive = 0;
-//        long result = xQueueReceive(queue, &receive, 2000 / portTICK_PERIOD_MS);
-//        if (result){
-//            printf("Iniciando Processamento - Dado recebido: %d \n", receive);
-//        }
-//    }
-//}
+void procTask(void * params){
+    while(1){
+        int receive = 0;
+        long result = xQueueReceive(queue, &receive, 2000 / portTICK_PERIOD_MS); //xQueueReceive retorna o valor enviado por xQueueSend
+        if (result){
+            printf("Iniciando Processamento - Dado recebido: %d \n", receive);
+        }
+    }
+}
 
 void app_main(void)
 {
     queue = xQueueCreate(4,sizeof(int));
     xTaskCreate(&comTask,"Task de comuniocação",2048,NULL,2,NULL);
-    //xTaskCreate(&procTask,"Task de execução"   ,2048,NULL,1,NULL);
+    xTaskCreate(&procTask,"Task de execução"   ,2048,NULL,1,NULL);
 }
